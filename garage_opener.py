e352a598d92907bc460b395664415b8c0604076b
skylark_projects <87293579+skylarkprojects@users.noreply.github.com>
2021-09-13 13:52:34 +1200

    Create garage_opener.py

---
+++ garage_opener.py
@@ -0,0 +1,32 @@
+# Using a Pin with micropython
+# Make sure you have the LED checkbox marked!
+
+import machine
+
+import time
+
+class switch:
+    
+    def __init__(self, pin):
+        
+        self.pin = pin
+                
+    def garage_opener(self):
+        
+        print('Opening garage door!')
+        print('  Turning switch on')
+        self.pin(1)
+        
+        time.sleep(3)
+        
+        print('  Turning switch off')
+        self.pin(0)
+        
+        
+y12 = machine.Pin('Y12')          
+m = switch(y12)
+                
+m.garage_opener()
+
+
+
