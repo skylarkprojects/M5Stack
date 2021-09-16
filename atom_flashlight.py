22208be529d3d0def9a149c82da80e1a84596e37
skylark_projects <87293579+skylarkprojects@users.noreply.github.com>
2021-09-16 14:33:55 +1200

    Update atom_flashlight.py
    Added functions for flashlight and text to colours

--- atom_flashlight.py
+++ atom_flashlight.py
@@ -1,27 +1,54 @@
-import machine
-import neopixel
+from m5stack import *
+from m5ui import *
+from uiflow import *
 import time
+import machine, neopixel
 
-gpio = 27
-no_pixels = 1
+def change_rgb(colour, np):
+  
+  if colour == 'red':
+    np[0] = (64, 0, 0) # set to red, full brightness
+    np.write()
+    
+  elif colour == 'green':
+    np[0] = (0, 64, 0) # set to green, half brightness
+    np.write()
+    
+  elif colour == 'blue':
+    np[0] = (0, 0, 64)  # set to blue, quarter brightness
+    np.write()
+    
+  else:
+    np[0] = (0, 0, 0)
+    np.write()
 
-pin = machine.Pin(gpio)
-x = 0
+def flashlights(np):
 
-while True
-        
-    np[0] = (255, 0 , 0)
-
-    time.sleep(1)
-    np[0] = (0, 255 , 0)
-
-    time.sleep(1)
-    np[0] = (0, 0 , 255)
+  colours = {
+        0 : 'red',
+        1 : 'blue'
+            }  
+            
+  x = 1
+  
+  while True:
     
     time.sleep(1)
-     x = x + 1
-     
-     if x == 20:
-         break
-        
+    change_rgb(colours[0],np)
+    time.sleep(1)
+    change_rgb(colours[1],np)    
     
+    x = x + 1
+    
+    if x >= 10:
+      break
+    
+
+
+pin = machine.Pin(27)
+bits = 1
+
+np = neopixel.NeoPixel(pin, bits)
+
+flashlights(np)
+
